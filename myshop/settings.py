2a6c8d2e3ab8de48b5a238d24f463be7c3e09d86
Czarina Catambing <Czarina.Catambing@canpotex.com>
2021-08-23 18:58:41 -0600

    made changes for deployment

--- myshop/settings.py
+++ myshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # SESSION_COOKIE_DOMAIN = 'mydomain.com'
 CART_SESSION_ID = 'cart'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+django_heroku.settings(locals())
